75ff81e37bd21a039ffae1bb191e42117d996096
Buğra Furkan Karadaş <bfK.2002@hotmail.com>
2025-11-21 21:10:12 +0300

    1-21 lessons

---
+++ Assets/Scripts/Collision.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       Debug.Log(collision.gameObject.name);     
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.gameObject.name);
+    }
+}
